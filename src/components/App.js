5a6989efe9aa4ccb66c0937d4f0022bf1122d07a
LJ <jhlee9193@gmail.com>
2020-12-05 22:27:30 +0900

    Router and Styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
-import "./App.css";
+import React from "react";
+import { HashRouter as Router } from "react-router-dom";
+import Home from "../routes/Home";
 
 function App() {
   return (
-    
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/:id" component={Detail} />
+    </Router>
   );
 }
 
